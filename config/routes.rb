d8b59bc9652ff9e5310d0d798efeda79de655c1c
muhammad-saadi <muhammad.saad@square63.com>
2022-10-05 14:27:37 +0500

    Replaced show with create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :slots, only: %w[index show]
+  resources :slots, only: %w[index create]
   root "slots#index"
 end
