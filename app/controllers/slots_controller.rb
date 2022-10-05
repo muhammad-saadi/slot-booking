4aeacbb148a7d0d55b27d05d56edf046696ccd2b
muhammad-saadi <muhammad.saad@square63.com>
2022-10-05 14:26:45 +0500

    handled all the possible cases for slot-booking

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,8 +1,19 @@
 class SlotsController < ApplicationController
   def index
-    @slots = Slot.all
+    @available_slots = Slot.available_slots(params[:date], params[:duration])
+
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
   def create
+    start_time = "#{params[:date]} #{params[:slot][:start].split('-').first}".to_datetime
+    end_time = start_time + params[:duration].to_i.minutes
+    @slot = Slot.new(start: start_time, end: end_time)
+    if @slot.save
+      redirect_to slots_path, :flash => { :success => "Your Slot has been booked successfully" }
+    end
   end
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,2 +1,34 @@
 class Slot < ApplicationRecord
+  def self.available_slots(selected_date, duration)
+    if selected_date.present? && duration.present?
+      selected_date = selected_date.to_datetime
+      duration = duration.to_i
+      booked_slots = Slot.where(start: selected_date..selected_date.end_of_day).order('start Asc').pluck(:start, :end)
+      available_slots = []
+      start_time = selected_date
+
+      booked_slots.each do |slot|
+        duration_between_slots = ((slot[0] - start_time)/1.minutes).to_i
+        available_slots = empty_slots(available_slots, start_time, slot[0], duration_between_slots, duration)
+        tart_time = slot[1]
+      end
+
+      duration_between_slots = (((selected_date.end_of_day - start_time.to_datetime)*24*60).to_i)
+      available_slots = empty_slots(available_slots, start_time, selected_date.end_of_day, duration_between_slots, duration)
+    end
+
+    available_slots
+  end
+
+  def self.empty_slots(available_slots, start_time, end_time, duration_between_slots, duration)
+    if duration_between_slots >= duration
+      counter = 0
+
+      while(start_time + (15*counter).minutes < end_time)
+        available_slots.push("#{(start_time + (15*counter).minutes).strftime("%H:%M")} - #{(start_time + (15*counter +duration).minutes).strftime("%H:%M")}")
+        counter+=1
+      end
+    end
+    available_slots
+  end
 end
